cf27f42357afa8142d511aac41be2bc9165a89a0
valerij <acopich@gmail.com>
2014-04-07 20:18:19 +0400

    Merge branch 'master' of volga:tm
    Conflicts:
    	src/brick/NonRobustBrick.scala
    	src/brick/RobustBrick.scala
    	src/documents/Alphabet.scala
    	src/documents/Document.scala
    	src/documents/Numerator.scala
    	src/documents/TextualDocument.scala
    	src/matrix/MatrixForSparsifier.scala
    	src/matrix/Ogre.scala
    	src/plsa/PLSA.scala
    	src/plsa/PLSAFactory.scala
    	src/regularizer/Regularizer.scala
    	src/scripts/PigData.scala
    	src/stoppingcriteria/MaxNumberOfIterationStoppingCriteria.scala
    	src/utils/ModelParameters.scala

---
+++ src/stoppingcriteria/MaxNumberOfIterationStoppingCriteria.scala
@@ -0,0 +1,22 @@
+package stoppingcriteria
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: padre
+ * Date: 24.03.14
+ * Time: 17:52
+ */
+/**
+ *
+ * @param maxNumberOfIteration number of iteration to stop
+ */
+class MaxNumberOfIterationStoppingCriteria(private val maxNumberOfIteration: Int) extends StoppingCriteria {
+    /**
+     *
+     * @param numberOfIteration the number of iteration that has been already done.
+     * @param oldPerplexity perplexity after the previous iteration.
+     * @param newPerplexity perplexity after this iteration
+     * @return true if iterationCnt >= maxNumberOfIteration
+     */
+    def apply(numberOfIteration: Int, oldPerplexity: Double, newPerplexity: Double): Boolean = numberOfIteration >= maxNumberOfIteration
+}
