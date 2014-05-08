933d0bfccac6c76a675a927609628b247a19f575
padre <kozlov-ilya@ispras.ru>
2014-05-08 19:28:38 +0400

    add more coments in quickstart

--- src/main/scala/scripts/QuickStart.scala
+++ src/main/scala/scripts/QuickStart.scala
@@ -6,7 +6,7 @@ import java.io.File
 import builder.PLSABuilder
 import java.util.Random
 import utils.TopicHelper
-import attribute.{Word, Category}
+import attribute.Category
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,9 +18,9 @@ import attribute.{Word, Category}
 object QuickStart extends App{
 
     /**
-     * first of all we have to read documents.TextualDocument
-     * that are sequences of words(Strings)
-     * We read TextualDocument from a textual file, one document per line. Words are separated by space.
+     * first of all we have to read textual documents.
+     * Textual document is a sequence of words(String)
+     * We read textual documents from a textual file, one document per line. Words are separated by space.
      * Documents should be preprocessed.
      */
     def getTextualDocuments(): Iterator[TextualDocument] = {
@@ -35,15 +35,15 @@ object QuickStart extends App{
         val wordsSequence = lines.map(line => line.split(" "))
 
         /**
-         * now we obtain a sequence of sequence of words and should to construct textual main.scala.documents.
+         * now we obtain a sequences of sequence of words and should to construct textual documents.
          * Textual document may contain a few texts, corresponding to different  attributes,
          * for example text in english, translation of this text to russian etc. If you document contain only one text
-         * you may use main.scala.attribute Category
+         * you may use attribute Category
          */
-        val textualDocuments = wordsSequence.map(words => new TextualDocument(Map(Word("en") -> words)))
+        val textualDocuments = wordsSequence.map(words => new TextualDocument(Map(Category -> words)))
 
         /**
-         * and now we return sequence of textual main.scala.documents
+         * and now we return sequence of textual documents
          */
         textualDocuments
     }
@@ -51,41 +51,44 @@ object QuickStart extends App{
 
 
     /**
-     * read textual main.scala.documents from file (see functions getTextualDocuments for details)
+     * read textual documents from file (see functions getTextualDocuments for details)
      */
     val textualDocuments = getTextualDocuments()
 
     /**
      * now we have to replace words by it serial number. For this purposes we would use object Numerator
      * Numerator take into input iterator of TextualDocuments, replace words by it serial numbers and return
-     * sequence of main.scala.documents and instance of class alphabet (alphabet hold map from wordsNumber to word and vice versa)
+     * sequence of documents and instance of class alphabet (alphabet hold map from wordsNumber to word and vice versa)
      */
     val (documents, alphabet) = Numerator(textualDocuments)
 
 
     /**
-     * now we have to build model. In this example we would use a main.scala.plsa
-     * we use main.scala.builder to build instance of class PLSA
-     * it require define number of topics, number of iterations, alphabet, sequence of main.scala.documents and random number generator
+     * now we have to build model. In this example we would use a plsa
+     * we use builder to build instance of class PLSA
+     * it require define number of topics, number of iterations, alphabet, sequence of documents and random number generator
      * to generate initial approximation
      */
     val numberOfTopics = 25
-    val numberOfIterations = 100 // number of iteration in EM algorithm
+    val numberOfIteration = 100 // number of iteration in EM algorithm
     val random = new Random() // java.util.Random
-    val builder = new PLSABuilder(numberOfTopics, alphabet, documents, random,  numberOfIterations)
+    val builder = new PLSABuilder(numberOfTopics, alphabet, documents, random,  numberOfIteration)
 
     /**
-     * and now we build main.scala.plsa
+     * and now we build plsa
      */
     val plsa  = builder.build()
 
     /**
-     * now we have documents and model and we may train model
+     * now we have documents and model and we may train model. Our model take into input sequence of documents and
+     * perform stochastic matrix decomposition F ~ Phi * Theta where Phi is distribution of words by topics and
+     * Theta is distribution of document by topic. The result would be saved in TrainedModel
      */
     val trainedModel = plsa.train(documents)
 
     /**
      * now we obtain matrix of distribution of words by topics and we may see most popular words from each topic
+     * For this porpose we use util printAllTopics. It print n words with the highest probability from every topic.
      */
     val n = 10 // number of top words to see
     TopicHelper.printAllTopics(n, trainedModel.phi(Category), alphabet)
