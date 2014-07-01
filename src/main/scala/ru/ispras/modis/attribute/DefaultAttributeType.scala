43c7a5c59f66b6452037013a98b05bba8c6d52be
valerij <acopich@gmail.com>
2014-07-01 18:38:47 +0400

    DefaultAttributeType.scala and SingleAttributeNumerator.scala for those of us who don't want to mess around with AttributeType

---
+++ src/main/scala/ru/ispras/modis/attribute/DefaultAttributeType.scala
@@ -0,0 +1,9 @@
+package ru.ispras.modis.attribute
+
+/**
+ * Created by valerij on 7/1/14.
+ */
+/**
+ * It's used implicitly if user has only a single Attribute Type and does not explicitly specify it
+ */
+object DefaultAttributeType extends AttributeType
